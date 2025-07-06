fbe893d669be1e2e04b36b8975e7e8ece435c7ef
kumarmurugan1218@gmail.com <kumarmurugan1218@gmail.com>
2025-07-06 12:15:27 +0530

    ♻️ Updated Weather App code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = ({menu,setMenu,list,arr}) => {
     
    {menu&&<nav className='absolute top-0 left-0 h-screen w-[400px] z-10 bg-white shadow-lg p-10 '>
       <ul className='grid w-full space-y-10 mt-3 '>
-        <li><NavLink className='links ' to={`/`}>Home</NavLink></li>
-        <li><NavLink className='links' to="/product">products</NavLink></li>
-        <li><NavLink className='links' to="/About">About</NavLink></li>
-        <li><NavLink className='links' to="/Contact">Contact</NavLink></li>
+        <li><NavLink className='links ' to='/'>Home</NavLink></li>
+        <li><NavLink className='links' to="product">products</NavLink></li>
+        <li><NavLink className='links' to="About">About</NavLink></li>
+        <li><NavLink className='links' to="Contact">Contact</NavLink></li>
         
       </ul>
       <button className='absolute top-5 right-7 cancel-btn' onClick={()=>{
@@ -22,9 +22,9 @@ const Navbar = ({menu,setMenu,list,arr}) => {
      <nav className='hidden space-x-5 py-4 shadow-md w-screen md:block'>
       <ul className='flex justify-center space-x-5'>
         <li ><NavLink className='links ' to="/">Home</NavLink></li>
-        <li><NavLink className='links' to="/product">Products</NavLink></li>
-        <li><NavLink className='links' to="/About">About</NavLink></li>
-        <li><NavLink className='links' to="/Contact">Contact</NavLink></li>
+        <li><NavLink className='links' to="product">Products</NavLink></li>
+        <li><NavLink className='links' to="About">About</NavLink></li>
+        <li><NavLink className='links' to="Contact">Contact</NavLink></li>
       </ul>
        
     </nav>
